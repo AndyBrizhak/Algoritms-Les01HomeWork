373e288d8be6fd29efe0a156e4d6831d9a201969
AndyBrizhak <andrewbrizhak@gmail.com>
2018-04-13 10:19:50 +0300

    +автоморфные числа в диапазоне,  формат беззнаковый целый, буду переделывать на лонг лонг

--- Les01HomeWork/AlgpritmsLes01Homework.c
+++ Les01HomeWork/AlgpritmsLes01Homework.c
@@ -24,7 +24,7 @@ struct Cell   //создаем структуру клетка
 
 typedef struct Cell ChessCell; // определяем ChessCell как стурктуру Cell
 
-void solution1();   // прототип функции
+void solution1();   // прототип функции Check if the cells have the same color
 void solution2();
 void solution3();
 void menu();
@@ -69,6 +69,52 @@ void checkColor(/*struct */ChessCell firstCell, /*struct*/ ChessCell secondCell)
 	}
 
 }
+
+int compareSpec(unsigned int head1, unsigned int head2)
+{
+	//unsigned int numDig = 0;
+	//unsigned int temp;
+	//temp = head1;
+	//while (temp>0)
+	//{
+	//	numDig += 1;
+	//	temp = temp / 10;
+	//}
+
+
+	unsigned int tail1 = NULL;
+	unsigned int tail2 = NULL;
+	unsigned int automorf = NULL;
+	unsigned int statehead1;
+	statehead1 = head1;
+	while (head1 > 0)													// пока больше нуля выполняем цикл
+	{
+		tail1 = tailing(head1);
+		head1 = head1 / 10;											// отсекаем хвост, оставляем голову	
+		tail2 = tailing(head2);											// вычисляем хвост - остаток от деления второго числа
+		head2 = head2 / 10;											// отсекаем хвост, оставляем голову	
+
+	 if (tail1 == tail2)
+		{
+		 automorf = statehead1;
+		}
+		else
+		{
+			automorf = NULL;											// иначе обнуляем значение автоморфного числа
+			break;														// прервываем цикл проверки автоморфного числа
+		}
+	}
+
+	return automorf;
+}
+
+unsigned int tailing(unsigned int head)
+{
+	unsigned int tail;
+	tail = head - (head / 10) * 10;											// вычисляем хвост - остаток от деления 
+	return tail;
+}
+
 
 int main()
 {
@@ -122,8 +168,42 @@ void solution1()
 
 void solution2()
 {
+	unsigned int num;
+	unsigned int i;
+	unsigned int result;
+	unsigned int automorf;
+
 	printf("Solution 2\n");
+	printf("Display all special numbers on the screen.\n ");
 	// Решение
+	printf("Input integer number: \n ");
+	scanf_s("%d", &num);
+	automorf = NULL;
+	for (i = 0; i <= num; i++)
+	{
+		if (i==0)
+		{
+			automorf = 0;
+			printf(" Automorf special number: \n ");
+			printf("%d\n ", automorf);
+		}
+		else if (i == 1)
+		{
+			automorf = 1;
+		}
+		else
+		{
+			result = i * i;
+			automorf = compareSpec(i, result);
+		}
+		
+		if (automorf != NULL)
+		{
+			printf(" Automorf special number: \n ");
+			printf("%d\n ", automorf);
+		}
+		
+	}
 }
 
 void solution3()
